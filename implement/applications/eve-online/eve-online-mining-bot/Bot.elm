52ffeb30130fb818bd1f8c928415d4aeff77572c
Sergei Gening <serge.gening@gmail.com>
2024-06-27 15:17:55 +0200

    tt

--- implement/applications/eve-online/eve-online-mining-bot/Bot.elm
+++ implement/applications/eve-online/eve-online-mining-bot/Bot.elm
@@ -819,7 +819,7 @@ modulesToActivateAlwaysActivated context inventoryWindowWithMiningHoldSelected =
                                                                 Just inactiveModule ->
                                                                     describeBranch "I see an inactive mining module. Activate it."
                                                                         (clickModuleButtonButWaitIfClickedInPreviousStep context inactiveModule) 
-                                                                        |> Maybe.withDefault startDroneMining
+                                                                        |> Maybe.withDefault startDroneMining context
                                                         }
                                                         
                                                     )
